c36dd7e108ad0d7714b12382bd417ca1699b4a90
t.libs <tlibs@hotmail.com>
2026-05-11 23:05:12 -0400

    chore: initial project scaffold

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>GardenCastle</h1>
+      <div id="game-container"></div>
+    </div>
+  );
+}
+
+export default App;
